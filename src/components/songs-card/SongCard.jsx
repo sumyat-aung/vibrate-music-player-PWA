76f04a4ee5fb8c42d63bbe22847bc6ba0f183830
sumyataung2004 <sumyataung1584@gmail.com>
2022-12-01 23:12:08 +0630

    songs-cart

--- src/components/songs-card/SongCard.jsx
+++ src/components/songs-card/SongCard.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const SongCard = ({ song }) => {
   console.log(song);
   return (
-    <div className="flex flex-col w-[250px] p-4 bg-opacity-80 animate-slideup rounded-lg m-5">
+    <SongTitleStyling className="flex flex-col w-[250px] p-4 bg-opacity-80 animate-slideup rounded-lg m-5">
       <div className="relative w-full">
         {/* <div
           className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${
@@ -28,15 +29,15 @@ const SongCard = ({ song }) => {
               ? song.images?.coverart
               : "https://cdn.shopify.com/s/files/1/0361/0781/3004/products/orange_0637efbb-de32-476b-8061-856d2b770d98_150x150.png?v=1660785252"
           }
-          className="w-full h-full rounded-lg  cursor-pointer"
+          className="w-full h-full rounded-3xl cursor-pointer"
         />
       </div>
 
       <div className="mt-4 flex flex-col">
-        <p className="font-semibold text-lg text-white truncate">
+        <p className="font-semibold text-lg text-white truncate tracking-wide">
           <Link to={`/songs/${song?.key}`}>{song.title}</Link>
         </p>
-        <p className="text-sm truncate text-gray-300 mt-1">
+        <p className="text-sm truncate text-gray-300 mt-1 tracking-wide">
           <Link
             to={
               song.artists
@@ -48,8 +49,12 @@ const SongCard = ({ song }) => {
           </Link>
         </p>
       </div>
-    </div>
+    </SongTitleStyling>
   );
 };
 
 export default SongCard;
+
+const SongTitleStyling = styled.div`
+  font-family: "Montserrat Alternates", sans-serif;
+`;
