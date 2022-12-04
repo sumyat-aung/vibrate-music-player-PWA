f95a8d63601166f11cf7e596cdd3c4450c50bda1
sumyataung2004 <sumyataung1584@gmail.com>
2022-12-05 00:40:02 +0630

    details responsive

--- src/pages/Details/ArtistDetail.jsx
+++ src/pages/Details/ArtistDetail.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useGetArtistDetailsQuery } from "../../redux/data/Songs";
 
 import Error from "../../components/common/Error";
@@ -13,23 +13,10 @@ const ArtistDetail = () => {
 
   let { artistsid } = useParams();
 
-  ///////////////////////////////////////////
+  const { data, isError, isFetching } = useGetArtistDetailsQuery({ artistsid });
 
-  // for development request ^^
-  localStorage.setItem(
-    "artistsData",
-    JSON.stringify(useGetArtistDetailsQuery({ artistsid }))
-  );
-  const { data, isError, isFetching } = JSON.parse(
-    localStorage.getItem("artistsData")
-  );
-  // for development request ^^
-
-  ///////////////////////////////////////////
-
-  // const { data, isError, isFetching } = useGetArtistDetailsQuery({ artistsid });
-
-  ///////////////////////////////////////////
+  // navigating
+  const navigate = useNavigate();
 
   // getting state from redux
   const { activeSong, isPlaying } = useSelector((state) => state.player);
@@ -37,10 +24,6 @@ const ArtistDetail = () => {
   let songs = data && Object.values(data?.songs);
   let albums = data && Object.values(data?.albums);
   let artists = data && Object.values(data?.artists);
-
-  console.log(songs);
-  // console.log(albums);
-  // console.log(artists);
 
   return (
     <div>
@@ -57,9 +40,9 @@ const ArtistDetail = () => {
       )}
 
       {data && (
-        <div className="flex h-[100vh]">
-          <ArtistsDetailSongs className="w-1/2 overflow-auto">
-            <div className="h-2/5 flex flex-col justify-center items-center">
+        <div className="flex flex-col lg:flex-row lg:h-[100vh] relative">
+          <ArtistsDetailSongs className="xl:w-1/2 lg:w-[40%] w-full overflow-auto">
+            <div className="h-[500px] flex flex-col justify-center items-center">
               <div className="relative">
                 <img
                   className="rounded-full w-[300px]"
@@ -80,8 +63,8 @@ const ArtistDetail = () => {
               <h1 className="text-2xl text-gray-30 text-gray-200 font-semibold tracking-wide sticky top-0 z-50 bg-main p-5">
                 {artists[0]?.attributes?.name}'s Songs
               </h1>
-              <div className="flex flex-wrap justify-center">
-                {songs.map((song, i) => (
+              <div className="flex flex-wrap justify-center overflow-auto">
+                {songs.map((song) => (
                   <ArtistDetailSongsCard d={song} />
                 ))}
               </div>
@@ -90,7 +73,48 @@ const ArtistDetail = () => {
 
           {/* -------------------- */}
 
-          <div className="w-1/2 border overflow-auto">f</div>
+          <AlbumsStyling className="xl:w-1/2 lg:w-[60%] w-full overflow-auto">
+            <h1 className="text-2xl text-gray-30 text-gray-200 font-semibold tracking-wide sticky top-0 z-20 bg-main py-5 px-10">
+              {artists[0]?.attributes?.name}'s Albums
+            </h1>
+            <div className="flex flex-col mt-10 sm:px-10 px-1 justify-center wrapper">
+              {albums.map((al) => (
+                <div className="bg-sb_bg rounded shadow flex my-5">
+                  <div className="relative">
+                    <img
+                      className="rounded w-[250px] h-full object-cover "
+                      src={artists[0]?.attributes?.artwork?.url}
+                      alt={artists[0]?.attributes?.name}
+                    />
+                  </div>
+                  <div className="py-5 px-5 w-full">
+                    <h1 className="font-wide text-md sm:text-2xl text-white mb-2">
+                      {al?.attributes?.name}
+                    </h1>
+                    <h1 className="font-wide text-sm sm:text-xl text-gray-50">
+                      {al?.attributes?.artistName}
+                    </h1>
+                    <h1 className="font-wide text-xs lg:text-md  text-gray-400">
+                      Release Date : {al?.attributes?.releaseDate}
+                    </h1>
+                    <a
+                      href={al?.attributes?.url}
+                      target="_blank"
+                      className="font-wide text-md  text-gray-300 hover:translate-x-1 no-underline flex justify-end font-mono duration-500"
+                    >
+                      See More ⇀
+                    </a>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </AlbumsStyling>
+          <button
+            className="absolute top-5 left-5 cursor-default sm:cursor-pointer"
+            onClick={() => navigate(-1)}
+          >
+            <i className="fa-solid fa-arrow-left text-gray-50 text-xl "></i>
+          </button>
         </div>
       )}
     </div>
@@ -108,4 +132,8 @@ const ArtistsDetailSongs = styled.div`
       filter: blur(5px);
     }
   }
+`;
+
+const AlbumsStyling = styled.div`
+  font-family: "Montserrat Alternates", sans-serif;
 `;
